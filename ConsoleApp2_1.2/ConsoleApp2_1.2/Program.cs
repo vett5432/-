734c41ef9b3c266c5f7cfbdf8aa7315ff3bcd274
vett5432 <groliz29@gmail.com>
2025-10-22 18:08:18 +0300

    Add files via upload

---
+++ ConsoleApp2_1.2/ConsoleApp2_1.2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2_1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int emin = 0;
+            int emax = 0;
+            Random rnd = new Random();
+            Console.Write("размерность массива(n) = ");
+            int n = int.Parse(Console.ReadLine());
+            int[] a = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = rnd.Next(-99, 100);
+            }
+            Console.WriteLine("исходный массив: ");
+            foreach (int elem in a)
+            {
+                Console.Write("{0} ", elem);
+            }
+            for (int i = 1; i < a.Length; i++)
+            {
+                if (a[i] < a[emin])
+                    emin = i;
+                if (a[i] > a[emax])
+                    emax = i;
+            }
+
+            int temp = a[emin];
+            a[emin] = a[emax];
+            a[emax] = temp;
+
+            Console.WriteLine("\nизмененный массив: ");
+            foreach (int elem in a) 
+            {
+                Console.Write("{0} ",elem);
+            }
+
+
+        }
+    }
+}
---
+++ ConsoleApp2_1.2/ConsoleApp2_1.2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("ConsoleApp2_1.2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleApp2_1.2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("a76169a7-10e1-4dc4-83ca-7adb2f35395f")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
